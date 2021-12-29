20146c5d8093fcef45b676f051a68518f87dbad1
Abdulsalam O. Odetayo <adeodetayo92@gmail.com>
2021-12-29 17:22:59 -0500

    Refactor, Feat: updated api endpoints to take params as body (#3)
    * Refactor, Feat: updated api endpoints to take params as body.
    
    * Feat: make sure no duplicate isbn number

--- src/server.js
+++ src/server.js
@@ -21,115 +21,260 @@ db.once("open", function () {
 });
 
 //serve static page on launch
-app.use("/", express.static('public'));
+app.use("/", express.static("public"));
 
 //GET request
-app.get("/book", function (req, res) {
-  Book.find({}, function (err, data) {
-    if (err) res.send(err);
-    else {
-      if (data.length === 0) res.send("Book does not exist");
-      else res.send(data);
-    }
-  });
-});
 app.get(
-  "/book/isbn:isbn?/title:title?",
+  "/:api_key/book",
+  /*
+    Check if api key exact match
+    sends back 401 error code if not match
+  */
   function (req, res, next) {
-    if (req.params.isbn && req.params.title) {
-      Book.find(
-        { isbn: req.params.isbn, title: req.params.title },
-        function (err, data) {
-          if (err) res.send(err);
-          else {
-            if (data.length === 0) res.send("Book does not exist");
-            else res.send(data);
-          }
-        }
-      );
-    } else next();
+    if (req.params.api_key === process.env.API_KEY) next();
+    else res.status(401).send({ message: "Unauthorized, Invalid API key." });
   },
+  /*
+      returns all books in the database
+  */
+  function (req, res) {
+    Book.find({}, function (err, data) {
+      if (err) res.status(404).send({ error: err });
+      else {
+        if (data.length === 0) res.status(200).send("Book does not exist");
+        else res.status(200).send(data);
+      }
+    });
+  }
+);
+app.get(
+  "/:api_key/book/find",
+  bodyParser.json(),
+  /*
+    Check if api key exact match
+    sends back 401 error code if not match
+  */
   function (req, res, next) {
-    if (req.params.isbn) {
-      Book.find({ isbn: req.params.isbn }, function (err, data) {
-        if (err) res.send(err);
+    if (req.params.api_key === process.env.API_KEY) next();
+    else res.status(401).send({ message: "Unauthorized, Invalid API key." });
+  },
+  /*  
+    finds book with isbn number and title
+  */
+  function (req, res, next) {
+    if (req.body.isbn && req.body.title) {
+      Book.find({ isbn: req.body.isbn, title: req.body.title }, (err, data) => {
+        if (err) return res.status(404).send({ error: err });
         else {
-          if (data.length === 0) res.send("Book does not exist");
-          else res.send(data);
+          if (data.length === 0)
+            return res.status(200).send({ message: "Book does not exist" });
+          else return res.status(200).send(data);
         }
       });
     } else next();
   },
+  /*  
+    finds book with isbn number
+  */
   function (req, res, next) {
-    if (req.params.title) {
-      Book.find({ title: req.params.title }, function (err, data) {
-        if (err) res.send(err);
+    if (req.body.isbn) {
+      Book.find({ isbn: req.body.isbn }, (err, data) => {
+        if (err) return res.status(404).send({ error: err });
         else {
-          if (data.length === 0) res.send("Book does not exist");
-          else res.send(data);
+          if (data.length === 0)
+            return res.status(200).send({ message: "Book does not exist" });
+          else return res.status(200).send(data);
+        }
+      });
+    } else next();
+  },
+  /*  
+    finds book with title
+  */
+  function (req, res, next) {
+    if (req.body.title) {
+      Book.find({ title: req.body.title }, (err, data) => {
+        if (err) return res.status(404).send({ error: err });
+        else {
+          if (data.length === 0)
+            return res.status(200).send({ message: "Book does not exist" });
+          else return res.status(200).send(data);
         }
       });
     } else next();
   },
   function (req, res) {
-    res.send("Book does not exist");
+    res.status(400).send({ message: "Bad Request." });
   }
 );
 
 //POST request
 app.post(
-  "/book/add/:isbn/:title/:author/:publisher/:pages",
-  function (req, res) {
-    let newBook = new Book({
-      isbn: req.params.isbn,
-      title: req.params.title,
-      author: req.params.author,
-      publisher: req.params.publisher,
-      pages: req.params.pages,
-    });
-    newBook
-      .save()
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => console.log("Error: \n" + err));
-  }
-);
-app.post(
-  "/book/add/:isbn/:title/:author/:publisher/:pages/:publish",
-  function (req, res) {
-    let newBook = new Book({
-      isbn: req.params.isbn,
-      title: req.params.title,
-      author: req.params.author,
-      publisher: req.params.publisher,
-      pages: req.params.pages,
-      publish: req.params.publish,
-    });
+  "/:api_key/book/add",
+  bodyParser.json(),
+  /*
+    Check if api key exact match
+    sends back 401 error code if not match
+  */
+  function (req, res, next) {
+    if (req.params.api_key === process.env.API_KEY) next();
+    else res.status(401).send({ message: "Unauthorized, Invalid API key." });
+  },
+  function (req, res, next) {
+    if (req.body.isbn) {
+      Book.find({ isbn: req.body.isbn }, (err, data) => {
+        if (err) return res.status(404).send({ error: err });
+        else {
+          if (data.length > 0)
+            return res.status(200).send({ message: "Book exist", data: data });
+          return next();
+        }
+      });
+    } else next();
+  },
+  /*
+      add book into the api with custom publish date
+  */
+  function (req, res, next) {
+    if (
+      req.body.isbn &&
+      req.body.title &&
+      req.body.author &&
+      req.body.publisher &&
+      req.body.pages &&
+      req.body.publish
+    ) {
+      let newBook = new Book({
+        isbn: req.body.isbn,
+        title: req.body.title,
+        author: req.body.author,
+        publisher: req.body.publisher,
+        pages: req.body.pages,
+        publish: req.body.publish,
+      });
 
-    newBook
-      .save()
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => console.log("Error: \n" + err));
+      newBook
+        .save()
+        .then((data) => {
+          res
+            .status(201)
+            .send({ message: "book was created successfully", data: data });
+        })
+        .catch((err) =>
+          res.status(400).send({ message: "Bad Request.", error: err })
+        );
+    } else next();
+  },
+  /*
+      add book into the api without custom publish date
+  */
+  function (req, res, next) {
+    if (
+      req.body.isbn &&
+      req.body.title &&
+      req.body.author &&
+      req.body.publisher &&
+      req.body.pages
+    ) {
+      let newBook = new Book({
+        isbn: req.body.isbn,
+        title: req.body.title,
+        author: req.body.author,
+        publisher: req.body.publisher,
+        pages: req.body.pages,
+      });
+      newBook
+        .save()
+        .then((data) => {
+          res
+            .status(201)
+            .send({ message: "book was created successfully", data: data });
+        })
+        .catch((err) => {
+          res.status(400).send({ message: "Bad Request.", error: err });
+        });
+    } else next();
+  },
+  function (req, res) {
+    res.status(400).send({ message: "Bad Request." });
   }
 );
 // PUT request
-app.put("/book/update/isbn:isbn", bodyParser.json(), function (req, res) {
-  Book.findOneAndUpdate(
-    { isbn: req.params.isbn },
-    req.body,
-    function (err, data) {
-      if (err) console.log(err);
-      else res.send(data);
-    }
-  );
-});
+app.put(
+  "/:api_key/book/update",
+  bodyParser.json(),
+  /*
+    Check if api key exact match
+    sends back 401 error code if not match
+  */
+  function (req, res, next) {
+    if (req.params.api_key === process.env.API_KEY) next();
+    else res.status(401).send({ message: "Unauthorized, Invalid API key." });
+  },
+  /*
+    If book exists in database, updates Book with matching isbn number from database
+  */
+  function (req, res, next) {
+    if (req.body.isbn) {
+      Book.findOneAndUpdate(
+        { isbn: req.body.isbn },
+        req.body,
+        function (err, data) {
+          if (err) return res.status(404).send({ error: err });
+          else {
+            if (data === null)
+              return res.status(200).send({ message: "Book does not exist" });
+            else
+              return res
+                .status(200)
+                .send({ message: "Book was updated successfully" });
+          }
+        }
+      ).catch((err) =>
+        res.status(400).send({ message: "Bad Request.", error: err })
+      );
+    } else next();
+  },
+  function (req, res) {
+    res.status(400).send({ message: "Bad Request." });
+  }
+);
 // DELETE request
-app.delete("/book/delete/isbn:isbn", function (req, res) {
-  Book.deleteOne({ isbn: req.params.isbn });
-});
+app.delete(
+  "/:api_key/book/delete",
+  bodyParser.json(),
+  /*
+    Check if api key exact match
+    sends back 401 error code if not match
+  */
+  function (req, res, next) {
+    if (req.params.api_key === process.env.API_KEY) next();
+    else res.status(401).send({ message: "Unauthorized, Invalid API key." });
+  },
+  /*
+    If book exists in database Deletes Book with matching isbn number from database
+  */
+  function (req, res, next) {
+    if (req.body.isbn) {
+      Book.deleteOne({ isbn: req.body.isbn }, (err, data) => {
+        if (err) return res.status(404).send({ error: err });
+        else {
+          if (data.deletedCount === 0)
+            return res.status(200).send({ message: "Book does not exist" });
+          else
+            return res
+              .status(200)
+              .send({ message: "Book was deleted successfully" });
+        }
+      }).catch((err) =>
+        res.status(400).send({ message: "Bad Request.", error: err })
+      );
+    } else next();
+  },
+  function (req, res) {
+    res.status(400).send({ message: "Bad Request." });
+  }
+);
 
 app.listen(port, () => {
   console.log("Listening on port " + port + "...................");
