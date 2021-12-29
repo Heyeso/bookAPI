d7af29833a2ecd34ec68ad6550657b728a504df4
Abdulsalam Oetayo <adeodetayo92@gmail.com>
2021-12-29 17:21:21 -0500

    Feat: make sure no duplicate isbn number

--- src/server.js
+++ src/server.js
@@ -26,10 +26,17 @@ app.use("/", express.static("public"));
 //GET request
 app.get(
   "/:api_key/book",
+  /*
+    Check if api key exact match
+    sends back 401 error code if not match
+  */
   function (req, res, next) {
     if (req.params.api_key === process.env.API_KEY) next();
     else res.status(401).send({ message: "Unauthorized, Invalid API key." });
   },
+  /*
+      returns all books in the database
+  */
   function (req, res) {
     Book.find({}, function (err, data) {
       if (err) res.status(404).send({ error: err });
@@ -43,10 +50,17 @@ app.get(
 app.get(
   "/:api_key/book/find",
   bodyParser.json(),
+  /*
+    Check if api key exact match
+    sends back 401 error code if not match
+  */
   function (req, res, next) {
     if (req.params.api_key === process.env.API_KEY) next();
     else res.status(401).send({ message: "Unauthorized, Invalid API key." });
   },
+  /*  
+    finds book with isbn number and title
+  */
   function (req, res, next) {
     if (req.body.isbn && req.body.title) {
       Book.find({ isbn: req.body.isbn, title: req.body.title }, (err, data) => {
@@ -59,6 +73,9 @@ app.get(
       });
     } else next();
   },
+  /*  
+    finds book with isbn number
+  */
   function (req, res, next) {
     if (req.body.isbn) {
       Book.find({ isbn: req.body.isbn }, (err, data) => {
@@ -71,6 +88,9 @@ app.get(
       });
     } else next();
   },
+  /*  
+    finds book with title
+  */
   function (req, res, next) {
     if (req.body.title) {
       Book.find({ title: req.body.title }, (err, data) => {
@@ -92,10 +112,29 @@ app.get(
 app.post(
   "/:api_key/book/add",
   bodyParser.json(),
+  /*
+    Check if api key exact match
+    sends back 401 error code if not match
+  */
   function (req, res, next) {
     if (req.params.api_key === process.env.API_KEY) next();
     else res.status(401).send({ message: "Unauthorized, Invalid API key." });
   },
+  function (req, res, next) {
+    if (req.body.isbn) {
+      Book.find({ isbn: req.body.isbn }, (err, data) => {
+        if (err) return res.status(404).send({ error: err });
+        else {
+          if (data.length > 0)
+            return res.status(200).send({ message: "Book exist", data: data });
+          return next();
+        }
+      });
+    } else next();
+  },
+  /*
+      add book into the api with custom publish date
+  */
   function (req, res, next) {
     if (
       req.body.isbn &&
@@ -126,6 +165,9 @@ app.post(
         );
     } else next();
   },
+  /*
+      add book into the api without custom publish date
+  */
   function (req, res, next) {
     if (
       req.body.isbn &&
@@ -161,10 +203,17 @@ app.post(
 app.put(
   "/:api_key/book/update",
   bodyParser.json(),
+  /*
+    Check if api key exact match
+    sends back 401 error code if not match
+  */
   function (req, res, next) {
     if (req.params.api_key === process.env.API_KEY) next();
     else res.status(401).send({ message: "Unauthorized, Invalid API key." });
   },
+  /*
+    If book exists in database, updates Book with matching isbn number from database
+  */
   function (req, res, next) {
     if (req.body.isbn) {
       Book.findOneAndUpdate(
@@ -182,7 +231,7 @@ app.put(
           }
         }
       ).catch((err) =>
-        res.status(400).send({ message: "Bad Request.", error: err})
+        res.status(400).send({ message: "Bad Request.", error: err })
       );
     } else next();
   },
@@ -194,10 +243,17 @@ app.put(
 app.delete(
   "/:api_key/book/delete",
   bodyParser.json(),
+  /*
+    Check if api key exact match
+    sends back 401 error code if not match
+  */
   function (req, res, next) {
     if (req.params.api_key === process.env.API_KEY) next();
     else res.status(401).send({ message: "Unauthorized, Invalid API key." });
   },
+  /*
+    If book exists in database Deletes Book with matching isbn number from database
+  */
   function (req, res, next) {
     if (req.body.isbn) {
       Book.deleteOne({ isbn: req.body.isbn }, (err, data) => {
